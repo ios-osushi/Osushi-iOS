59b1aebdef955ab6d9a2af645b53502e44f007ac
KaitoMuraoka <kaitoboshi@icloud.com>
2024-09-29 23:01:11 +0900

    SwiftLint のwarningを修正

--- Osushi/View/InfomationList/SNSView.swift
+++ Osushi/View/InfomationList/SNSView.swift
@@ -5,7 +5,9 @@ struct SNSView: View {
     let gitHubUrlString: String
     
     var body: some View {
+        // swiftlint:disable force_unwrapping
         Link(Strings.Infomation.x, destination: URL(string: xUrlString)!)
         Link(Strings.Infomation.gitHub, destination: URL(string: gitHubUrlString)!)
+        // swiftlint:enable force_unwrapping
     }
 }
--- Osushi/View/PostList/PostListViewModel.swift
+++ Osushi/View/PostList/PostListViewModel.swift
@@ -22,12 +22,10 @@ final class PostListViewModel: ObservableObject {
 
     func descriptText(_ content: String) -> String {
         let lines = content.components(separatedBy: "\n")
-        for line in lines {
-            if line.starts(with: "description:") {
-                return line
-                    .replacingOccurrences(of: "description:", with: "")
-                    .trimmingCharacters(in: .whitespaces)
-            }
+        for line in lines where line.starts(with: "description:") {
+            return line
+                .replacingOccurrences(of: "description:", with: "")
+                .trimmingCharacters(in: .whitespaces)
         }
         return Strings.PostList.errorMessage
     }
